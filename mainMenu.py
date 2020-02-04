8d2c43f99705f9b137893e0929f4e889ecac5b75
Yousuke Utsumi <youtsumi@gmail.com>
2020-02-04 15:59:07 -0800

    Fixed spelling

--- mainMenu.py
+++ mainMenu.py
@@ -3,12 +3,12 @@ from os import path
 import os
 import json
 
-class VaccumMonitor(Display):
+class VacuumMonitor(Display):
     def __init__(self, parent=None, args=None, macros=None):
         dir_path = os.path.dirname(os.path.realpath(__file__)).replace('\\','\\\\')
         macros=dict(launcher=args[0],path=dir_path)
         #macros = dict(path=dir_path)
-        super(VaccumMonitor, self).__init__(parent=parent, macros=macros)
+        super(VacuumMonitor, self).__init__(parent=parent, macros=macros)
 
         self.configs=dict()
 
